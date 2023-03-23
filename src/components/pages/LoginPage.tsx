62c8e71c5f507dc77b73f228040d0b333733f9ba
rvanasa <ryan.vandersmith@dfinity.org>
2023-03-22 21:25:10 -0600

    Set up basic Auth0 integration

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,8 +1,15 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import tw from 'twin.macro';
+
+const LoginButton = tw.div`p-3 border-2 text-xl rounded-xl cursor-pointer`;
+
 export default function LoginPage() {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <>
-      <div className="d-flex w-full h-screen">
-        <h1>Login!</h1>
+      <div tw="flex flex-col items-center mt-10">
+        <LoginButton onClick={() => loginWithRedirect()}>Login</LoginButton>
       </div>
     </>
   );
