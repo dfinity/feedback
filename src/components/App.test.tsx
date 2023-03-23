da5b1c004b25b9fa55f3b2809325bf48c7d709e1
rvanasa <ryan.vandersmith@dfinity.org>
2023-03-23 10:24:42 -0600

    Fix front-end test imports

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,5 +1,5 @@
 // import { render, screen } from '@testing-library/react';
-// import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 // import App from './App';
 
 describe('App', () => {
