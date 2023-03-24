65163b63f49e63e7b3fa22fea08ca16668c6abb5
rvanasa <ryan.vandersmith@dfinity.org>
2023-03-23 18:27:04 -0600

    Misc

---
+++ src/components/pages/SubmitPage.tsx
@@ -0,0 +1,11 @@
+// import { useFeedbackStore } from '../../stores/feedbackStore';
+
+export default function SubmitPage() {
+  // const create = useFeedbackStore((state) => state.create);
+
+  return (
+    <>
+      <div tw="flex flex-col items-center p-10 bg-white rounded-xl"></div>
+    </>
+  );
+}
