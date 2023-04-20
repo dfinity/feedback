668e64758b5cc413a4c536afebeb3de0160f15e2
rvanasa <ryan.vandersmith@dfinity.org>
2023-04-20 16:32:52 -0600

    Progress

--- src/components/pages/QueuePage.tsx
+++ src/components/pages/QueuePage.tsx
@@ -3,19 +3,23 @@ import { Topic, useTopicStore } from '../../stores/topicStore';
 import TopicView from '../TopicView';
 import { useEffect, useState } from 'react';
 import { handlePromise } from '../../utils/handlers';
+import { useIdentityStore } from '../../stores/identityStore';
 
 export default function QueuePage() {
   const [topics, setTopics] = useState<Topic[]>([]);
 
+  const user = useIdentityStore((state) => state.user);
   const getModeratorQueue = useTopicStore((state) => state.getModeratorQueue);
 
   useEffect(() => {
-    handlePromise(
-      getModeratorQueue(),
-      'Fetching topics...',
-      'Error while fetching topics!',
-    ).then((topics) => setTopics(topics));
-  }, [getModeratorQueue]);
+    if (user) {
+      handlePromise(
+        getModeratorQueue(),
+        'Fetching topics...',
+        'Error while fetching topics!',
+      ).then((topics) => setTopics(topics));
+    }
+  }, [getModeratorQueue, user]);
 
   return (
     <>
