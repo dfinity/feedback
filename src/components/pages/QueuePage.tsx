3f1c3ea85f37875dc28e5c029adccaae7ef10770
rvanasa <ryan.vandersmith@dfinity.org>
2023-04-20 22:16:41 -0600

    Progress

--- src/components/pages/QueuePage.tsx
+++ src/components/pages/QueuePage.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
+import { FaCheck, FaTimes } from 'react-icons/fa';
 import tw from 'twin.macro';
 import { useIdentityStore } from '../../stores/identityStore';
 import { ModStatus, Topic, useTopicStore } from '../../stores/topicStore';
-import { handleError, handlePromise } from '../../utils/handlers';
-import TopicView from '../TopicView';
-import { FaCheck, FaTimes } from 'react-icons/fa';
+import { handleError } from '../../utils/handlers';
 import Loading from '../Loading';
+import TopicView from '../TopicView';
 
 const ModeratorButton = tw.div`flex items-center gap-2 font-bold px-3 py-3 text-xl rounded-full cursor-pointer border-2 bg-[#fffd] border-gray-300 hover:scale-105`;
 
@@ -18,11 +18,11 @@ export default function QueuePage() {
 
   useEffect(() => {
     if (user) {
-      handlePromise(
-        getModeratorQueue(),
-        'Fetching topics...',
-        'Error while fetching topics!',
-      ).then((topics) => setTopics(topics));
+      getModeratorQueue()
+        .then((topics) => setTopics(topics))
+        .catch((err) =>
+          handleError(err, 'Error while fetching moderator queue!'),
+        );
     }
   }, [getModeratorQueue, user]);
 
@@ -41,6 +41,11 @@ export default function QueuePage() {
 
   return (
     <>
+      {topics.length === 0 && (
+        <div tw="bg-gray-100 text-xl text-center px-2 py-5 rounded-xl text-gray-600 select-none">
+          All caught up!
+        </div>
+      )}
       <div tw="flex flex-col gap-4 mx-auto">
         {topics.map((topic) => (
           <div key={topic.id}>
