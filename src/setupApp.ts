e7914900b5353f2f12b85299c669ec578121547b
rvanasa <ryan.vandersmith@dfinity.org>
2024-03-04 15:04:48 -0700

    Fix page routing

--- src/setupApp.ts
+++ src/setupApp.ts
@@ -1,17 +1,28 @@
-import { HttpAgent } from '@dfinity/agent';
+import { Actor } from '@dfinity/agent';
 import { backend } from './declarations/backend';
+import { getNetwork } from './utils/network';
 
 export const applicationName = 'ICP Developer Feedback';
 
-const agent = (backend as any)[Symbol.for('ic-agent-metadata')].config
-  .agent as HttpAgent;
-if (import.meta.env.PROD) {
-  (agent as any)._host = 'https://icp0.io/';
-}
-
+// Redirect to custom domain
 if (
   window.location.hostname.endsWith('.icp0.io') ||
   window.location.hostname.endsWith('.ic0.app')
 ) {
   window.location.hostname = 'dx.internetcomputer.org';
+}
+
+// Patch `@dfinity/agent`
+const agent = Actor.agentOf(backend);
+if (getNetwork() === 'ic') {
+  (agent as any)._host = 'https://icp-api.io/';
+}
+
+// Redirect to localhost URL for improved page routing
+const dfxPort = 4943;
+const url = new URL(window.location.href);
+const canisterId = url.searchParams.get('canisterId');
+if (canisterId && url.port === String(dfxPort)) {
+  url.searchParams.delete('canisterId');
+  window.location.href = `http://${canisterId}.localhost:${dfxPort}?${url.searchParams}`;
 }
